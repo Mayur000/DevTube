d103e8f62d14635ce593dc8ec5a921a072c68d92
MAYUR <mayurnarkhede18@outlook.com>
2025-02-16 00:55:31 +0530

    express app.js(cors,cookie-parser) asyncHandler ApiError ApiResponce

--- src/index.js
+++ src/index.js
@@ -4,5 +4,19 @@ dotenv.config({
 });
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("errror", (error) => {
+      console.log("ERRR: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo connection failed !!! ", err);
+  });
